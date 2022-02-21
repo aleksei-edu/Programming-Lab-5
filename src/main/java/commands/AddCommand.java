808cf1aa83fab2d1661a64bde2ac34ce8ed9104d
Alexey <a.lapin03@gmail.com>
2022-02-21 21:08:31 +0300

    second commit

---
+++ src/main/java/commands/AddCommand.java
@@ -0,0 +1,28 @@
+package commands;
+
+import exception.CommandNeedArgumentException;
+import exception.CommandNotAcceptArgumentsException;
+import utility.CollectionManager;
+import utility.CreateNewElementManager;
+
+public class AddCommand extends AbstractCommand{
+    public AddCommand(){
+        super("add","добавить новый элемент в коллекцию");
+    }
+
+    @Override
+    public void execute(String argument) {
+        try {
+            if(!argument.isEmpty()) throw new CommandNotAcceptArgumentsException();
+            try{
+                CreateNewElementManager.add();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        catch(CommandNotAcceptArgumentsException e){
+            e.printStackTrace();
+        }
+    }
+}
